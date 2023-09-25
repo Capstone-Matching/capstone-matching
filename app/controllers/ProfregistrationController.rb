6b027d9052c18eb4a246563d29638019d0dc1644
Hunter_Zacha <hunterz7890@gmail.com>
2023-09-25 15:28:17 -0500

    trying to rebase again

--- app/controllers/ProfregistrationController.rb
+++ app/controllers/ProfregistrationController.rb
@@ -1,6 +1,9 @@
 class ProfregistrationController < ApplicationController
   def index
+<<<<<<< HEAD
     # This is where you can display the registration form
+=======
+>>>>>>> 94009b8727de525cbdcbedf358e87a2e69730fcf
   end
 
   def create
@@ -8,15 +11,27 @@ class ProfregistrationController < ApplicationController
     email = params[:email]
     first_name = params[:first_name]
     last_name = params[:last_name]
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> 94009b8727de525cbdcbedf358e87a2e69730fcf
     if email.end_with?("tamu.edu")
       #check if duplicate
         #error duplicate
         #flash[:error] = "Already registered, wait for admin response before trying again"
+<<<<<<< HEAD
+=======
+        #redirect_to profregistration_path and return
+>>>>>>> 94009b8727de525cbdcbedf358e87a2e69730fcf
       #else
         #add data to database
       flash[:success] = "Registration Successful! Please wait for the admin to approve your registration for access."
       redirect_to profregistration_path and return
+<<<<<<< HEAD
+=======
+      #end
+>>>>>>> 94009b8727de525cbdcbedf358e87a2e69730fcf
     else
       flash[:error] = "Not a valid tamu.edu email address."
       redirect_to profregistration_path and return
