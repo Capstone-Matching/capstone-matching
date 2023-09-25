c347e52105fdd838bb50688725fb27095e059b79
Hunter_Zacha <hunterz7890@gmail.com>
2023-09-25 01:29:32 -0500

    verified input, added messages for success/fail

--- app/controllers/ProfregistrationController.rb
+++ app/controllers/ProfregistrationController.rb
@@ -1,4 +1,25 @@
 class ProfregistrationController < ApplicationController
-    def index
+  def index
+    # This is where you can display the registration form
+  end
+
+  def create
+    # Extract form parameters
+    email = params[:email]
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    
+    if email.end_with?("tamu.edu")
+      #check if duplicate
+        #error duplicate
+        #flash[:error] = "Already registered, wait for admin response before trying again"
+      #else
+        #add data to database
+      flash[:success] = "Registration Successful! Please wait for the admin to approve your registration for access."
+      redirect_to profregistration_path and return
+    else
+      flash[:error] = "Not a valid tamu.edu email address."
+      redirect_to profregistration_path and return
     end
+  end
 end
